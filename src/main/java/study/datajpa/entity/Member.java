f86cd60cab7d5adf177827bbb95801b7a7dab111
tddudwns1 <tjddudwns1@naver.com>
2024-01-15 13:04:49 +0900

    2-1. 예제 도메인 모델과 동작확인
    1. team entity 생성
    기존 member entity와 연관 관계
    2. 롬복 설명
       @Setter: 실무에서 가급적 Setter는 사용하지 않기
       @NoArgsConstructor AccessLevel.PROTECTED: 기본 생성자 막고 싶은데, JPA 스팩상
       PROTECTED로 열어두어야 함
       @ToString은 가급적 내부 필드만(연관관계 없는 필드만
    3. changeTeam()
    로 양방향 연관관계 한번에 처리(연관관계 편의 메소드, Member.team 이 연관관계의 주인)

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,24 +1,40 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = PROTECTED)
+@ToString(of = {"id", "username", "age"}) // team 적으면 큰일남(무한루프)
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null)
+            changeTeam(team);
     }
 }
---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,31 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
